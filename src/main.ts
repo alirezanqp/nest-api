a70f1d8725972b0885b23c0f80dc7e51133132bf
alirezanqp <alirezanqp@gmail.com>
2023-01-02 21:18:44 +0330

    user fastify with nest and enable global validation pips

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+// import fastifyHelmet from '@fastify/helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // await app.register(fastifyHelmet);
+  await app.listen(5000);
 }
 bootstrap();
